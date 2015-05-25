2265831209d24f7426f7eb7a44f8bd5e53e2f4fd
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-05-25 22:51:14 +0200

    add timeout for normal fetch as well

--- config-wrapper/src/main/java/com/u6f6o/apps/cfgw/AutoRefreshingConfigMap.java
+++ config-wrapper/src/main/java/com/u6f6o/apps/cfgw/AutoRefreshingConfigMap.java
@@ -16,19 +16,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * TODO: logging
  */
 public class AutoRefreshingConfigMap extends ReadOnlyMap {
-    private static final long MAX_STARTUP_TIME = 10; // sec
-    private static final long REFRESH_PERIOD = 10; // sec
+    private static final long MAX_STARTUP_TIME = 100000; // sec
+    private static final long REFRESH_PERIOD = 100000; // sec
 
     private final ConfigFetcher configFetcher;
     private final AtomicBoolean canRefresh;
+    private final ExecutorService timeoutExecutor;
     private final ScheduledExecutorService refreshExecutor;
 
-    private volatile ImmutableMap<String, String> propsCache;
+    private volatile ImmutableMap<String, String> configCache;
 
 
     private AutoRefreshingConfigMap(ConfigFetcher configFetcher) {
         this.configFetcher = configFetcher;
         this.canRefresh = new AtomicBoolean(true);
+        this.timeoutExecutor = Executors.newSingleThreadExecutor();
         this.refreshExecutor = Executors.newSingleThreadScheduledExecutor();
     }
 
@@ -40,100 +42,116 @@ public class AutoRefreshingConfigMap extends ReadOnlyMap {
     }
 
 
+
     /**
      * Supposed to be run during application startup. MAX_STARTUP_TIME indicates how long to
-     * wait for the configuration to be loaded.
+     * wait for the configuration to be initially loaded.
      *
-     * @throws java.lang.IllegalStateException in case of any error or timeout exceeded
+     * @throws java.lang.AssertionError in case configuration cannot be initially loaded
      */
     private void initializeConfig() {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<Map<String, String>> future = executor.submit(new Callable<Map<String, String>>() {
-            @Override
-            public Map<String, String> call() throws Exception {
-                return configFetcher.fetchLatestConfig();
-            }
-        });
-
-        try {
-            Map<String, String> freshConfig = future.get(MAX_STARTUP_TIME, TimeUnit.SECONDS);
-            propsCache = ImmutableMap.copyOf(freshConfig);
-        } catch (TimeoutException e) {
-            future.cancel(true);
-            throw new IllegalStateException("Properties initialization failed, because of timeout", e);
-        } catch (Exception e) {
-            throw new IllegalStateException("Properties initialization failed, aborting.", e);
-        } finally {
-            try {
-                executor.shutdown();
-                executor.awaitTermination(5, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                executor.shutdownNow();
-            }
+        Map<String, String> freshConfig = fetchWithTimeout(MAX_STARTUP_TIME);
+        if(!validateConfig(freshConfig)) {
+            throw new AssertionError("Application config could not be initially loaded, aborting!");
         }
+        publishNewConfig(freshConfig);
     }
+
 
     /**
      * Registers a task that repeatedly refreshes the cached configuration by a given delay,
      * REFRESH_PERIOD. In case something goes wrong, the old values are kept.
      */
     private void scheduleConfigRefresh() {
-        refreshExecutor.scheduleAtFixedRate(new Runnable() {
+        refreshExecutor.scheduleWithFixedDelay(new Runnable() {
             @Override
             public void run() {
                 if (canRefresh.compareAndSet(true, false)) {
                     try {
-                        Map<String, String> fetchedProps = configFetcher.fetchLatestConfig();
-                        propsCache = ImmutableMap.copyOf(fetchedProps);
+                        Map<String, String> freshConfig = fetchWithTimeout(REFRESH_PERIOD); // TODO WRONG
+                        if (!validateConfig(freshConfig)) {
+                            throw new IllegalStateException("Cannot refresh configuration, old values are kept!");
+                        }
+                        publishNewConfig(freshConfig);
                     } catch (Exception e) {
-                        // TODO do sth, ffs! Logging should be enough here
+                        // TODO MEANINGFUL LOGGING
                         e.printStackTrace();
                     } finally {
                         canRefresh.set(true);
                     }
                 }
             }
-        }, REFRESH_PERIOD, REFRESH_PERIOD, TimeUnit.SECONDS);
+        }, REFRESH_PERIOD, REFRESH_PERIOD, TimeUnit.MILLISECONDS);
     }
 
+    /**
+     * BLOCKING!!!
+     * @param timeout
+     * @return
+     */
+    private Map<String, String> fetchWithTimeout(long timeout) {
+        Future<Map<String, String>> future = timeoutExecutor.submit(new Callable<Map<String, String>>() {
+            @Override
+            public Map<String, String> call() throws Exception {
+                return configFetcher.fetchLatestConfig();
+            }
+        });
+        try {
+            Map<String, String> freshConfig = future.get(timeout, TimeUnit.MILLISECONDS);
+            return freshConfig;
+        } catch (Exception e) {
+            // TODO LOGGING
+            future.cancel(true);
+            return null;
+        }
+    }
+
+    // TODO make hookable
+    private boolean validateConfig(Map<String, String> freshConfig) {
+        return freshConfig != null && freshConfig.size() > 0;
+    }
+
+    private void publishNewConfig(Map<String, String> freshConfig) {
+        configCache = ImmutableMap.copyOf(freshConfig);
+    }
 
     @Override
     public int size() {
-        return propsCache.size();
+        return configCache.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return propsCache.isEmpty();
+        return configCache.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return propsCache.containsKey(key);
+        return configCache.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return propsCache.containsValue(value);
+        return configCache.containsValue(value);
     }
 
     @Override
     public String get(Object key) {
-        return propsCache.get(key);
+        return configCache.get(key);
     }
 
     @Override
     public Set<String> keySet() {
-        return propsCache.keySet();
+        return configCache.keySet();
     }
 
     @Override
     public Collection<String> values() {
-        return propsCache.values();
+        return configCache.values();
     }
 
     @Override
     public Set<Entry<String, String>> entrySet() {
-        return propsCache.entrySet();
+        return configCache.entrySet();
     }
 }
