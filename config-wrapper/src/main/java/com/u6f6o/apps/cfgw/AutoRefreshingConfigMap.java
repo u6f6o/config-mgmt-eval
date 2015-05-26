9d55db14fd51c614b6ca20590fec7b5cda1249b2
Ulf Gitschthaler <ulf.gitschthaler@payon.com>
2015-05-26 11:56:02 +0200

    refactoring, add logging

--- config-wrapper/src/main/java/com/u6f6o/apps/cfgw/AutoRefreshingConfigMap.java
+++ config-wrapper/src/main/java/com/u6f6o/apps/cfgw/AutoRefreshingConfigMap.java
@@ -2,26 +2,28 @@ package com.u6f6o.apps.cfgw;
 
 import com.google.common.collect.ImmutableMap;
 import com.u6f6o.apps.cfgw.provider.ConfigFetcher;
+import com.u6f6o.apps.cfgw.util.TimeSpan;
+import org.apache.log4j.Logger;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * TODO: consider using FutureTask
- * TODO: add timeout also for config update
- * TODO: proper error handling
- * TODO: logging
  */
 public class AutoRefreshingConfigMap extends ReadOnlyMap {
-    private static final long MAX_STARTUP_TIME = 100000; // sec
-    private static final long REFRESH_PERIOD = 100000; // sec
+    private static final Logger LOGGER = Logger.getLogger(AutoRefreshingConfigMap.class);
+
+    private static final TimeSpan MAX_STARTUP_TIME = TimeSpan.seconds(10);
+    private static final TimeSpan REFRESH_PERIOD = TimeSpan.seconds(10);
 
     private final ConfigFetcher configFetcher;
     private final AtomicBoolean canRefresh;
-    private final ExecutorService timeoutExecutor;
     private final ScheduledExecutorService refreshExecutor;
 
     private volatile ImmutableMap<String, String> configCache;
@@ -30,8 +32,7 @@ public class AutoRefreshingConfigMap extends ReadOnlyMap {
     private AutoRefreshingConfigMap(ConfigFetcher configFetcher) {
         this.configFetcher = configFetcher;
         this.canRefresh = new AtomicBoolean(true);
-        this.timeoutExecutor = Executors.newSingleThreadExecutor();
-        this.refreshExecutor = Executors.newSingleThreadScheduledExecutor();
+        this.refreshExecutor = Executors.newScheduledThreadPool(2); // 1 x fetch + 1 x timeout
     }
 
     public static AutoRefreshingConfigMap newConsulCatalogue(ConfigFetcher configFetcher) {
@@ -42,12 +43,11 @@ public class AutoRefreshingConfigMap extends ReadOnlyMap {
     }
 
 
-
     /**
-     * Supposed to be run during application startup. MAX_STARTUP_TIME indicates how long to
-     * wait for the configuration to be initially loaded.
+     * Load initial configuration settings during startup. In case it does not work, the application
+     * is supposed to stop immediately!
      *
-     * @throws java.lang.AssertionError in case configuration cannot be initially loaded
+     * @throws java.lang.AssertionError in case of timeout or failing validation
      */
     private void initializeConfig() {
         Map<String, String> freshConfig = fetchWithTimeout(MAX_STARTUP_TIME);
@@ -60,7 +60,11 @@ public class AutoRefreshingConfigMap extends ReadOnlyMap {
 
     /**
      * Registers a task that repeatedly refreshes the cached configuration by a given delay,
-     * REFRESH_PERIOD. In case something goes wrong, the old values are kept.
+     * REFRESH_PERIOD. In case the new configuration cannot be fetched, the old configuration
+     * is kept.
+     *
+     * This method does not throw any exception but logs in case something goes wrong. Failed
+     * refresh of the configuration properties must not harm the runnign application.
      */
     private void scheduleConfigRefresh() {
         refreshExecutor.scheduleWithFixedDelay(new Runnable() {
@@ -74,30 +78,29 @@ public class AutoRefreshingConfigMap extends ReadOnlyMap {
                         }
                         publishNewConfig(freshConfig);
                     } catch (Exception e) {
-                        // TODO MEANINGFUL LOGGING
-                        e.printStackTrace();
+                        LOGGER.error("Cannot refresh configuration, old values are kept!", e);
                     } finally {
                         canRefresh.set(true);
                     }
                 }
             }
-        }, REFRESH_PERIOD, REFRESH_PERIOD, TimeUnit.MILLISECONDS);
+        }, REFRESH_PERIOD.getSpan(), REFRESH_PERIOD.getSpan(), REFRESH_PERIOD.getUnit());
     }
 
     /**
-     * BLOCKING!!!
-     * @param timeout
-     * @return
+     * Fetches the latest configuration.
+     * @param timeout max wait time for the fetch operation
+     * @return latest configuration
      */
-    private Map<String, String> fetchWithTimeout(long timeout) {
-        Future<Map<String, String>> future = timeoutExecutor.submit(new Callable<Map<String, String>>() {
+    private Map<String, String> fetchWithTimeout(TimeSpan timeout) {
+        Future<Map<String, String>> future = refreshExecutor.submit(new Callable<Map<String, String>>() {
             @Override
             public Map<String, String> call() throws Exception {
                 return configFetcher.fetchLatestConfig();
             }
         });
         try {
-            Map<String, String> freshConfig = future.get(timeout, TimeUnit.MILLISECONDS);
+            Map<String, String> freshConfig = future.get(timeout.getSpan(), timeout.getUnit());
             return freshConfig;
         } catch (Exception e) {
             // TODO LOGGING
---
+++ config-wrapper/src/main/java/com/u6f6o/apps/cfgw/util/TimeSpan.java
@@ -0,0 +1,37 @@
+package com.u6f6o.apps.cfgw.util;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimeSpan {
+    private final TimeUnit unit;
+    private final long time;
+
+    private TimeSpan(TimeUnit unit, long time) {
+        this.unit = unit;
+        this.time = time;
+    }
+
+    public static TimeSpan seconds(long time) {
+        return new TimeSpan(TimeUnit.SECONDS, time);
+    }
+
+    public static TimeSpan minutes(long time) {
+        return new TimeSpan(TimeUnit.MINUTES, time);
+    }
+
+    public TimeUnit getUnit() {
+        return unit;
+    }
+
+    public long getSpan() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSpan{" +
+                "unit=" + unit +
+                ", time=" + time +
+                '}';
+    }
+}
