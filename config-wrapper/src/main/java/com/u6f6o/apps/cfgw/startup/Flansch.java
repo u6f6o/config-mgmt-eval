51f9485609368f15bb148ff3bafeac37583a21a0
Ulf Gitschthaler <ulf.gitschthaler@payon.com>
2015-05-26 17:28:29 +0200

    change fake main a bit

--- config-wrapper/src/main/java/com/u6f6o/apps/cfgw/startup/Flansch.java
+++ config-wrapper/src/main/java/com/u6f6o/apps/cfgw/startup/Flansch.java
@@ -1,8 +1,9 @@
 package com.u6f6o.apps.cfgw.startup;
 
 import com.u6f6o.apps.cfgw.AutoRefreshingConfigMap;
-import com.u6f6o.apps.cfgw.ReadOnlyMap;
 import com.u6f6o.apps.cfgw.provider.consul.ConsulConfigFetcher;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
 
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -13,8 +14,13 @@ public class Flansch {
         Map<String, String> props = AutoRefreshingConfigMap.newConsulCatalogue(new ConsulConfigFetcher());
 
         while(true) {
-            for (Map.Entry<String, String> entry : props.entrySet()) {
-                System.err.println(entry.getKey() + ": " + entry.getValue());
+            try {
+                for (Map.Entry<String, String> entry : props.entrySet()) {
+                    System.err.println(entry.getKey() + ": " + StringUtils.newStringUtf8(
+                            Base64.decodeBase64(entry.getValue())));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
             TimeUnit.SECONDS.sleep(20);
         }
