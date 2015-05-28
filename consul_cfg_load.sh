13aaafc442c75621d33ad870274d985f3c380392
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-05-28 22:27:50 +0200

    some minor shell script changes

---
+++ consul_cfg_load.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PROPS_FILE=$1
+
+cat $PROPS_FILE | dos2unix | grep -P '^\w' |  perl -lpe 's|^(\w.*?)=(.*)$|curl -X PUT -d '\''\2\'\'' http://localhost:8500/v1/kv/web/\1|' | source /dev/stdin
