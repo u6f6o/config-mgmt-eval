b115a192e728d416004ee074cda5a73e8d70f2d9
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-06-01 09:09:51 +0200

    replace consul by zookeeper

--- zookeeper/bin/zk_log.sh
+++ zookeeper/bin/zk_log.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-tail -f /var/tmp/consul_cluster.log | awk '
-	/consul-srv1/ {print "\033[31m" $0 "\033[39m"}
-	/consul-srv2/ {print "\033[32m" $0 "\033[39m"}
-	/consul-srv3/ {print "\033[33m" $0 "\033[39m"}
-	/consul-cli1/ {print "\033[34m" $0 "\033[39m"}
-	/consul-cli2/ {print "\033[35m" $0 "\033[39m"}
-	/consul-app1/ {print "\033[36m" $0 "\033[39m"}
-	/consul-app2/ {print "\033[36m" $0 "\033[39m"}
+tail -f /var/tmp/zookeeper_cluster.log | awk '
+	/zk-srv1/ {print "\033[31m" $0 "\033[39m"}
+	/zk-srv2/ {print "\033[32m" $0 "\033[39m"}
+	/zk-srv3/ {print "\033[33m" $0 "\033[39m"}
+	/zk-cli1/ {print "\033[34m" $0 "\033[39m"}
+	/zk-cli2/ {print "\033[35m" $0 "\033[39m"}
+	/zk-app1/ {print "\033[36m" $0 "\033[39m"}
+	/zk-app2/ {print "\033[36m" $0 "\033[39m"}
 '
--- zookeeper/bin/zk_start.sh
+++ zookeeper/bin/zk_start.sh
@@ -5,7 +5,7 @@
 ../config-wrapper/gradlew -p ../config-wrapper installDist
 cp -rf ../config-wrapper/build/install/config-wrapper dist/
 
-# stop and remove consul server and zkclient containers
+# stop and remove zookeeper server and zkclient containers
 docker rm -f zksrv1 zksrv2 zksrv3 zkcli1 zkcli2 zkapp1 zkapp2 >> /dev/null 2>&1
 
 # cleanup logfile
@@ -13,24 +13,24 @@ cat /dev/null > /var/tmp/zookeeper_cluster.log
 
 # recreate local container
 docker build -t zkapp -f AppFile . 
-docker build -t consul -f ConsulFile . 
+docker build -t zookeeper -f ConsulFile . 
 
-# 1st consul server node
-nohup docker run --name zksrv1 -h zksrv1 -p 8500:8500 consul -server -ui-dir /ui -config-dir /config -bootstrap-expect 3 2>&1 | sed -u 's/^/zksrv1] /' >> /var/tmp/consul_cluster.log &
+# 1st zookeeper server node
+nohup docker run --name zksrv1 -h zksrv1 -p 8500:8500 zookeeper -server -ui-dir /ui -config-dir /config -bootstrap-expect 3 2>&1 | sed -u 's/^/zk-srv1] /' >> /var/tmp/zookeeper_cluster.log &
 sleep 2
 JOIN_IP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' zksrv1)"
 
-# 2nd consul server node
-nohup docker run --name zksrv2 -h zksrv2 consul -server -join $JOIN_IP 2>&1 | sed -u 's/^/[zksrv2] /' >> /var/tmp/consul_cluster.log &
+# 2nd zookeeper server node
+nohup docker run --name zksrv2 -h zksrv2 zookeeper -server -join $JOIN_IP 2>&1 | sed -u 's/^/[zk-srv2] /' >> /var/tmp/zookeeper_cluster.log &
 sleep 2
 
-# 3rd consul server node
-nohup docker run --name zksrv3 -h zksrv3 consul -server -join $JOIN_IP 2>&1 | sed -u 's/^/[zksrv3] /' >> /var/tmp/consul_cluster.log &
+# 3rd zookeeper server node
+nohup docker run --name zksrv3 -h zksrv3 zookeeper -server -join $JOIN_IP 2>&1 | sed -u 's/^/[zk-srv3] /' >> /var/tmp/zookeeper_cluster.log &
 sleep 2
 
 # 2 client nodes 
-nohup docker run --name zkcli1 -h zkcli1 -p 8501:8500 consul -join $JOIN_IP 2>&1 | sed -u 's/^/[zkcli1] /' >> /var/tmp/consul_cluster.log &
-nohup docker run --name zkcli2 -h zkcli2 -p 8502:8500 consul -join $JOIN_IP 2>&1 | sed -u 's/^/[zkcli2] /' >> /var/tmp/consul_cluster.log &
+nohup docker run --name zkcli1 -h zkcli1 -p 8501:8500 zookeeper -join $JOIN_IP 2>&1 | sed -u 's/^/[zk-cli1] /' >> /var/tmp/zookeeper_cluster.log &
+nohup docker run --name zkcli2 -h zkcli2 -p 8502:8500 zookeeper -join $JOIN_IP 2>&1 | sed -u 's/^/[zk-cli2] /' >> /var/tmp/zookeeper_cluster.log &
 sleep 2
 
 CLIENT1_IP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' zkcli1)"
@@ -40,6 +40,6 @@ CLIENT2_IP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' zkcli2)"
 curl -X PUT -d 'bar' http://localhost:8500/v1/kv/zkapp/ps/foo
 sleep 2
 
-nohup docker run -e "CONSUL_CLIENT_IP=$CLIENT1_IP" --name zkapp1 -h zkapp1 zkapp 2>&1 | sed -u 's/^/[zkapp1] /' >> /var/tmp/consul_cluster.log &
-nohup docker run -e "CONSUL_CLIENT_IP=$CLIENT2_IP" --name zkapp2 -h zkapp2 zkapp 2>&1 | sed -u 's/^/[zkapp2] /' >> /var/tmp/consul_cluster.log &
+nohup docker run -e "ZOOKEEPER_CLIENT_IP=$CLIENT1_IP" --name zkapp1 -h zkapp1 zkapp 2>&1 | sed -u 's/^/[zk-app1] /' >> /var/tmp/zookeeper_cluster.log &
+nohup docker run -e "ZOOKEEPER_CLIENT_IP=$CLIENT2_IP" --name zkapp2 -h zkapp2 zkapp 2>&1 | sed -u 's/^/[zk-app2] /' >> /var/tmp/zookeeper_cluster.log &
 sleep 2
